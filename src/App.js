824ede865e2d2adac5ce88e03b5aafe5fdb4e3a1
Jaehyung Lee <jayhyung9@naver.com>
2025-04-01 22:32:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Box from "./component/Box";
 import './App.css';
 
+
+//1. 박스 2개 (타이틀, 사진, 결과)
+//2. 버튼 3개 (가위, 바위, 보)
+//3. 버튼과 사진 연동(버튼 클릭 시 사진 반영)
+//4. 컴퓨터는 랜덤하게 아이템이 선택
+//5. 3, 4번의 결과를 가지고 누가 이겼는지 승패 반영
+//6. 승패에 따라 박스 테두리, 글씨색 색 변경(이기면 초록, 지면 빨강, 비기면 검정)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box />
     </div>
   );
 }
---
+++ src/component/Box.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className="box">
+      <h1>you</h1>
+      <img className="item-image" src="https://m.media-amazon.com/images/I/51zbr3M5HVL.jpg"></img>
+      <h2>win</h2>
+      </div>
+  )
+}
+
+export default Box
