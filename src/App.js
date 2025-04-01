adf8d2cc668df83d8f4c67bfbc6a0c1e3701469f
Jaehyung Lee <jayhyung9@naver.com>
2025-04-02 01:31:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Box from "./component/Box";
 import './App.css';
+import {useState} from"react"
 
 
 //1. 박스 2개 (타이틀, 사진, 결과)
@@ -8,10 +9,41 @@ import './App.css';
 //4. 컴퓨터는 랜덤하게 아이템이 선택
 //5. 3, 4번의 결과를 가지고 누가 이겼는지 승패 반영
 //6. 승패에 따라 박스 테두리, 글씨색 색 변경(이기면 초록, 지면 빨강, 비기면 검정)
+
+const choice = {
+  rock : {
+name :"Rock",
+img :"https://m.media-amazon.com/images/I/61W8BVTF10L.jpg"
+  },
+  paper : {
+    name :"paper",
+    img :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5V6tWts93Od4sjC082k9DWFvCRK4NGUlfxg&s"
+  },
+  scissor : {
+    name :"scissor",
+    img :"https://m.media-amazon.com/images/I/51zbr3M5HVL.jpg"
+  }
+};
+
 function App() {
-  return (
+const [userSelect,setUserSelect] = useState(null)
+
+  const play=(userChoice)=>{
+    setUserSelect(choice[userChoice]);
+
+  };
+
+  return ( 
     <div>
-      <Box />
+    <div className="main">
+      <Box title="you" item={userSelect}/>
+      {/*< Box title="computer"/>*/}
+    </div>
+    <div className="main">
+      <button onClick={()=> play("scissor")}><i class="fa-solid fa-hand-scissors"></i></button>
+      <button onClick={()=> play("rock")}><i class="fa-solid fa-hand-back-fist"></i></button>
+      <button onClick={()=> play("paper")}><i class="fa-solid fa-note-sticky"></i></button>
+    </div>
     </div>
   );
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Box = () => {
+const Box = (props) => {
+  console.log("props", props)
   return (
     <div className="box">
-      <h1>you</h1>
-      <img className="item-image" src="https://m.media-amazon.com/images/I/51zbr3M5HVL.jpg"></img>
+      <h1>{props.title}</h1>
+      <img className="item-image" src={props.item && props.item.img} />
       <h2>win</h2>
       </div>
   )
